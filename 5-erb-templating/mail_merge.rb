e27b2e3382e48420a85d738e2ed0d6fc077ad00d
kdavidson <kdavidson@standardbredcanada.ca>
2021-10-04 13:01:53 -0400

    Fixed with fee calculation

--- 5-erb-templating/mail_merge.rb
+++ 5-erb-templating/mail_merge.rb
@@ -19,13 +19,23 @@ end
 
 template = File.read('templates/letter.txt.erb')
 
+# Dates come in as yyyy-MM-dd
+def parse_date(date_str)
+  split = date_str.split('-')
+  Date.new(split[0].to_i, split[1].to_i, split[2].to_i)
+end
+
+def calculate_fee(date, fee_per_day)
+  (Date.today - date).to_i * fee_per_day
+end
+
 CSV.foreach('data/overdue.csv') do |row|
   overdue = OverdueCheckout.new
   overdue.first_name = row[0].strip
   overdue.last_name = row[1].strip
   overdue.title = row[2].strip
-  overdue.due_date = row[3].strip
-  overdue.fee = row[4].strip
+  overdue.due_date = parse_date row[3].strip
+  overdue.fee = calculate_fee(overdue.due_date, 1)
 
   filename = "#{overdue.first_name}_#{overdue.last_name}.txt".downcase
   filepath = File.join('output', filename)
